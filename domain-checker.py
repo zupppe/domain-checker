2ff325838ee9f92226bc2b4452ae91a3559e6730
zupppe <131760599+zupppe@users.noreply.github.com>
2024-12-08 17:03:35 +0300

    Update domain-checker.py

--- domain-checker.py
+++ domain-checker.py
@@ -24,29 +24,17 @@ def main():
     for domain in domains:
         if check_domain_availability(domain):
             print(f"{domain} -> Boşta")
-            sil()
             print("\n{} Yeni Sorgu İçin {}+{}, geri dönmek için {}-".format(red, dark, red, dark))
-            if keyboard.is_pressed("+"):
-                sil()
-                main()
-            elif keyboard.is_pressed("-"):
-                sil()
-                chdir("..")
-                chdir("..")
-                sil()
-                system("python multitool.py")
         else:
             print(f"{domain} -> Meşgül")
-            print("\n{} Yeni Sorgu İçin {}+{}, geri dönmek için {}-".format(red, dark, red, dark))
-            if keyboard.is_pressed("+"):
+            anamenu = input("\n{} Yeni Sorgu İçin {}+{}, geri dönmek için {}-".format(red, dark, red, dark))
+            if anamenu == ("+"):
                 sil()
                 main()
-            elif keyboard.is_pressed("-"):
+            elif anamenu == ("-"):
                 sil()
                 chdir("..")
                 chdir("..")
-                sil()
                 system("python multitool.py")
-
 if __name__ == "__main__":
     main()
